d417763a068991bf495f6761f86c32ad9f9b57bd
Alexandra Weber <aweber@PepPop.medmicro.wisc.edu>
2014-04-16 13:50:05 -0500

    Progress as of 4/16

--- compareGFF3.py
+++ compareGFF3.py
@@ -20,3 +20,35 @@ if len(sys.argv) != 3:
     sys.exit()
 
 
+pCDS = IntervalSet []
+mCDS = IntervalSet []
+ptRNA = IntervalSet []
+mtRNA = IntervalSet []
+prRNA = IntervalSet []
+mtRNA = IntervalSet []
+
+infile = open(sys.argv[1], "r")
+
+#Separate genes into categories based on strand and feature type
+for line in infile:
+        line = line.strip()
+        entries = line.split()
+        strand = entries[7]
+        feature = entries[2]
+        start = entries[3]
+        stop = entires[4]
+
+        if strand == "+":
+                if feature == "CDS":
+                        pCDS.add(Interval(start,stop))
+                elif feature == "tRNA":
+                        ptRNA.add(Interval(start,stop))
+                else:
+                        prRNA.add(Interval(start,stop))
+        else:
+                if feature == "CDS":
+                        mCDS.add(Interval(start,stop))
+                elif feature == "tRNA":
+                        mtRNA.add(Interval(start,stop))
+                else:
+                        mrRNA.add(Interval(start,stop))
